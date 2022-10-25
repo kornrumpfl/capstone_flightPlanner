3d6425fbdf5bc51a7c3d3a20ebf13c15f61fc7c8
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-10-25 15:46:08 +0200

    - create a input field for the departure airport - create a input field for the arrival airport - create a dummy select box for the departure runaway - create a dummy select box for the arrival runaway

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,36 @@ export default function Main() {
   return (
     <section>
       <SectionFlightInfo>
-        <p>Flight number</p>
+        <h2>Flight number</h2>
         <input type="text" name="flightNumber"></input>
       </SectionFlightInfo>
       <AirportSelection>
-        <Departure></Departure>
-        <Arrival></Arrival>
+        <Departure>
+          <h2>Departure</h2>
+          <input
+            type="text"
+            name="departureAirport"
+            placeholder="Departure airport"
+          ></input>
+          <select>
+            <option value="RW5">RW 5</option>
+            <option value="RW15">RW 15</option>
+            <option value="RW23">RW 23</option>
+            <option value="RW33">RW 33</option>
+          </select>
+        </Departure>
+        <Arrival>
+          <h2>Arrival</h2>
+          <input
+            type="text"
+            name="arrivalAirport"
+            placeholder="Arrival airport"
+          ></input>
+          <select>
+            <option value="RW8">RW 8</option>
+            <option value="RW23">RW 26</option>
+          </select>
+        </Arrival>
       </AirportSelection>
     </section>
   );
@@ -18,10 +42,37 @@ export default function Main() {
 const SectionFlightInfo = styled.section`
   margin-top: 2vw;
   display: flex;
+  align-items: center;
   justify-content: space-evenly;
+  input {
+    max-height: 5em;
+  }
 `;
 
 const AirportSelection = styled.section`
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
 `;
-const Departure = styled.div``;
+const Departure = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  input {
+    max-width: 40vw;
+  }
+`;
+
+const Arrival = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  input {
+    max-width: 40vw;
+  }
+`;
